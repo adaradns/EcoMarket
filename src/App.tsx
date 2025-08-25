d7adef2039081eba89740cf4a605cf5f1502d047
adaradenis <adaradenis@gmail.com>
2025-08-25 20:12:48 -0300

    Prepare for GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductCard from './components/ProductCard';
 import FilterSidebar from './components/FilterSidebar';
 import CartSidebar from './components/CartSidebar';
